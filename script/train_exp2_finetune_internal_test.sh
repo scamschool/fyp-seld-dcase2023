131a2146d0bc25d2fde57d868534c6884280873b
Phua Wei An <PHUA0148@e.ntu.edu.sg>
2026-03-26 15:57:50 +0800

    Edited experiment 2 script

--- script/train_exp2_finetune_internal_test.sh
+++ script/train_exp2_finetune_internal_test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 # Experiment 2: Finetune on internal dataset, test on internal dataset
-# NOTE: Replace <TIMESTAMP> and <ITERATION> with actual values from Exp 1 output
-# e.g. params_20250101120000_0020000.pth
-EXP1_MODEL="./data_dcase2023_task3/model_monitor/exp1_starss_train_internal_test/<TIMESTAMP>/params_<TIMESTAMP>_0020000.pth"
+EXP1_MODEL="./data_dcase2023_task3/model_monitor/exp1_starss_train_internal_test/20260325121921/params_20260325121921_0020000.pth"
 
 for batch_size in 16; do
     CUDA_VISIBLE_DEVICES=0 python seld.py \
@@ -11,6 +9,6 @@ for batch_size in 16; do
     -s 1000 -i 20000 \
     -twt ./data_dcase2023_task3/list_dataset/internal_train.txt \
     -valwt ./data_dcase2023_task3/list_dataset/internal_test.txt \
-    -m ./data_dcase2023_task3/model_monitor/exp2_finetune_internal_test \
+    -m /home/weian001/docker_share/audio-visual-seld-dcase2023/data_dcase2023_task3/model_monitor/exp2_finetune_internal_test \
     -pm ${EXP1_MODEL};
 done
